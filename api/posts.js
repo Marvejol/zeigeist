bfc70a0c27306995d568006648dbef61635938e7
Ernesto Gemperle <ernestogemperle@Ernestos-MacBook-Air.local>
2025-01-17 19:09:17 +0100

    yolo

--- api/posts.js
+++ api/posts.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
 
 
 async function anonymizePost(post) {
-    const prompt = `give me back only the story but after having given all of the characters new names:"${post}"`;
+    const prompt = `give all of the characters new names "${post}", answer only with the result in ""`;
 
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
