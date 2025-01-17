f422602767e7a662d750733829f4a206a0ccaa6a
Ernesto Gemperle <ernestogemperle@MacBookAir.station>
2025-01-17 23:45:58 +0100

    almost there

--- api/posts.js
+++ api/posts.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
 
 
 async function anonymizePost(post) {
-    const prompt = `Please rewrite the following post, substituting each person’s name with a different, unique name from a large pool of all the possible names. Ensure that the same original name is replaced consistently throughout the post but avoid using the same few names repeatedly. Use a diverse set of names to maintain variety. Here’s the post: "${post}"`;
+    const prompt = `Please rewrite the following post, substituting each person’s name with a different, unique name from a large pool of all the possible names. Ensure that the same original name is replaced consistently throughout the post but avoid using the same few names repeatedly. The names you use this time should be different from what you would tipically use. Here’s the post: "${post}"`;
 
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
